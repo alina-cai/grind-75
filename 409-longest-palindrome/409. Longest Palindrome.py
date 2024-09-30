4e97bc19afd3e53c65ebbfe3bf2abaf2b4f1b89e
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-09-30 16:22:27 -0400

    Runtime: 45 ms (7.07%), Memory: 16.6 MB (67.72%) - Gitcode

--- 409-longest-palindrome/409. Longest Palindrome.py
+++ 409-longest-palindrome/409. Longest Palindrome.py
@@ -1,13 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        countS = Counter(s)
+        count = {}
         odd = 0
 
-        for ch, count in countS.items():
-            if count % 2 == 1:
+        for ch in s:
+            count[ch] = 1 + count.get(ch, 0)
+
+            if count[ch] % 2 == 1:
                 odd += 1
+            else:
+                odd -= 1
 
         if odd:
             return len(s) - odd + 1
-
+        
         return len(s)
