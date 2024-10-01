fd8dc39c71eae7a8bb2a4770193bcabb2e2e3bf3
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-09-30 20:03:51 -0400

    Runtime: 81 ms (21.06%), Memory: 20 MB (21.80%) - Gitcode

--- 57-insert-interval/57. Insert Interval.py
+++ 57-insert-interval/57. Insert Interval.py
@@ -3,11 +3,11 @@ class Solution:
         res = []
 
         for i in range(len(intervals)):
-            if intervals[i][0] > newInterval[1]:
+            if newInterval[1] < intervals[i][0]:
                 res.append(newInterval)
 
                 return res + intervals[i:]
-            elif intervals[i][1] < newInterval[0]:
+            elif newInterval[0] > intervals[i][1]:
                 res.append(intervals[i])
             else:
                 newInterval[0] = min(newInterval[0], intervals[i][0])
