374db348ffb6494ed0bcedbc435287522d677b3a
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-10-01 12:02:42 -0400

    Runtime: 38 ms (52.67%), Memory: 16.6 MB (25.20%) - Gitcode

--- 67-add-binary/67. Add Binary.py
+++ 67-add-binary/67. Add Binary.py
@@ -2,24 +2,25 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         n = max(len(a), len(b))
         res = ""
-        a, b = a.zfill(n), b.zfill(n)
         carry = 0
+
+        a, b = a.zfill(n), b.zfill(n)
 
         for i in range(n - 1, -1, -1):
             if a[i] == "1":
                 carry += 1
-
+            
             if b[i] == "1":
                 carry += 1
 
-            if carry % 2 == 1:
-                res += "1"
-            else:
+            if carry % 2 == 0:
                 res += "0"
+            else:
+                res += "1"
 
             carry //= 2
 
         if carry:
             res += "1"
-            
+
         return res[::-1]
