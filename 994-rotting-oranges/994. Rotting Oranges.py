079e3d5f8e6c7cd421caa98145424299a81aadcc
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-10-02 19:33:11 -0400

    Runtime: 55 ms (27.01%), Memory: 16.7 MB (5.35%) - Gitcode

--- 994-rotting-oranges/994. Rotting Oranges.py
+++ 994-rotting-oranges/994. Rotting Oranges.py
@@ -1,44 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-
-        def rot(r, c):
-            nonlocal fresh 
-            
-            if (
-                r not in range(ROWS) or
-                c not in range(COLS) or
-                grid[r][c] != 1
-            ):
-                return
-
-            grid[r][c] = 2
-            q.append([r, c])
-            fresh -= 1
-
-        q = deque()
         fresh = 0
+        q = deque()
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 elif grid[r][c] == 2:
-                    q.append([r, c])
+                    q.append((r, c))
 
         res = 0
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-        while fresh > 0 and q:
-            for i in range(len(q)):
+        while q and fresh > 0:
+            for _ in range(len(q)):
                 r, c = q.popleft()
-                
-                rot(r + 1, c)
-                rot(r - 1, c)
-                rot(r, c + 1)
-                rot(r, c - 1)
+
+                for nr, nc in directions:
+                    newR, newC = nr + r, nc + c
+
+                    if (
+                        newR in range(ROWS) and
+                        newC in range(COLS) and
+                        grid[newR][newC] == 1
+                    ):
+                        q.append((newR, newC))
+                        grid[newR][newC] = 2
+                        fresh -= 1
 
             res += 1
 
         return res if fresh == 0 else -1
-
-# O(m*n), O(m*n)
