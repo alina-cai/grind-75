8d64b946c1a000d8025d097eab158738e4ebd9e5
alina-cai <117325741+alina-cai@users.noreply.github.com>
2024-10-03 02:38:57 -0400

    Runtime: 43 ms (91.01%), Memory: 16.5 MB (89.01%) - Gitcode

--- 994-rotting-oranges/994. Rotting Oranges.py
+++ 994-rotting-oranges/994. Rotting Oranges.py
@@ -6,10 +6,10 @@ class Solution:
 
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+                if grid[r][c] == 2:
                     q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
 
         res = 0
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
